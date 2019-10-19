ae8515fd74417b74169a8cd18bb83626d4b5aeba
SahilShikalgar <shikalgar32@gmail.com>
2019-10-19 23:12:00 +0530

    Completed with applying react native basics

--- react-native-basics/App.js
+++ react-native-basics/App.js
@@ -1,25 +1,33 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalHandler = goalTitle => {
     // setCourseGoals([...courseGoals, enteredGoal]); can't give us gurantee of adding 
     setCourseGoals(currentGoals => [...courseGoals, {uid: Math.random().toString(), value: goalTitle}]);  // always work
+    setIsAddMode(false);  
+    //will set state change at once and re-render once
   }
-
+  
   const removeGoalHandler = goalId => {
     setCourseGoals(currentGoals => {
       return currentGoals.filter((goal) => goal.uid !== goalId);
     });
   }
 
+  const cancelGoalHandler = () => {
+    setIsAddMode(false);     
+  }
+
   return (
     <View style={styles.container}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button title="ADD NEW GOAL" onPress={() => setIsAddMode(true)} />
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalHandler} />
       <FlatList 
         data={courseGoals} 
         keyExtractor={(item) => item.uid}
