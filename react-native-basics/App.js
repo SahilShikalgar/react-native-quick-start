f6df6225b1bbf6214b53fd03347cb2ea42e5dc55
SahilShikalgar <shikalgar32@gmail.com>
2019-10-19 21:03:20 +0530

    App before adding modal for adding goal | Completed with basic things of react native

--- react-native-basics/App.js
+++ react-native-basics/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const addGoalHandler = goalTitle => {
+    // setCourseGoals([...courseGoals, enteredGoal]); can't give us gurantee of adding 
+    setCourseGoals(currentGoals => [...courseGoals, {uid: Math.random().toString(), value: goalTitle}]);  // always work
+  }
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.uid !== goalId);
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList 
+        data={courseGoals} 
+        keyExtractor={(item) => item.uid}
+        renderItem={itemData => (
+          <GoalItem
+            title={itemData.item.value}
+            onDeleteGoal={removeGoalHandler.bind(this, itemData.item.uid)}
+          />
+        )} 
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    padding: 50
+  }
 });
