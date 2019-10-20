de464940c3797fe95f49fb74cc9ea05208aa34f3
SahilShikalgar <shikalgar32@gmail.com>
2019-10-20 11:57:26 +0530

    Added new project - Guest a Number

--- react-native-basics/App.js
+++ react-native-basics/App.js
@@ -9,6 +9,9 @@ export default function App() {
 
   const addGoalHandler = goalTitle => {
     // setCourseGoals([...courseGoals, enteredGoal]); can't give us gurantee of adding 
+    if (goalTitle.length === 0) {
+      return;
+    }
     setCourseGoals(currentGoals => [...courseGoals, {uid: Math.random().toString(), value: goalTitle}]);  // always work
     setIsAddMode(false);  
     //will set state change at once and re-render once
